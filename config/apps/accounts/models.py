ba57303a7fc632a974c1f28ca11228b323430e8e
izaan@seaionl.com <izaan@seaionl.com>
2025-12-14 04:49:51 +0530

    refactor: add manager for better querying

--- config/apps/accounts/models.py
+++ config/apps/accounts/models.py
@@ -1,41 +1,11 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.db.models.functions import Lower
 from django.utils import timezone
 
+from .managers import UserManager
+
 # Create your models here.
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email: str, password: str | None = None, **extra_fields):
-        if not email:
-            raise ValueError('Email must be set')
-
-        normalized_email = self.normalize_email(email).strip().lower()
-        user = self.model(email=normalized_email, **extra_fields)
-
-        if password is None:
-            user.set_unusable_password()
-        else:
-            user.set_password(password)
-
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email: str, password: str, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        if not password:
-            raise ValueError('Superusers must have a password.')
-
-        return self.create_user(email=email, password=password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
